11dd7433e8832a4b9ce90586f5b75a5c1306ffeb
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-25 13:29:15 +0800

    Update method.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Router.php
+++ src/Router.php
@@ -5,6 +5,15 @@ namespace Swarm;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method \Symfony\Component\Routing\RouteCollection getRoutes()
+ * @method void addRoute(string $method, string $uri, string|object $action)
+ * @method void get(string $method, string|object $action)
+ * @method void post(string $method, string|object $action)
+ * @method void put(string $method, string|object $action)
+ * @method void patch(string $method, string|object $action)
+ * @method void delete(string $method, string|object $action)
+ * @method void webSocket(string $method, string|object $action)
+ *
  * @see \Swarm\Server\Router
  */
 class Router extends Facade
